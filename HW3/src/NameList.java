e74a63aa70879262f21aeb1560e677a7ec4ac748
yomyyo <samtang226@gmail.com>
2017-03-09 14:07:35 -0800

    Fully function list implementation, read comments

--- HW3/src/NameList.java
+++ HW3/src/NameList.java
@@ -10,6 +10,8 @@ public class NameList {
     private int totalOccurrences = 0;
     ArrayList<Node> peopleList;
 
+    //Change error message as desired
+    
     public NameList(String filename){
         peopleList = new ArrayList<>();
         try{
@@ -20,6 +22,8 @@ public class NameList {
         }
     }
 
+    //kept the ranking for each gender as seperate, however does not affect totalOccurrence count    
+    
     private void loadArrayList(String fileName)throws FileNotFoundException{
         int count = 0;
         Scanner scan = new Scanner(new File(fileName));
@@ -43,6 +47,8 @@ public class NameList {
         }
     }
 
+    //Used to sort for ranking in mostPopularName
+    
     private ArrayList<Node> sortRank(ArrayList<Node> aList){
         Node temp;
         for(int i = 1; i < aList.size(); i++){
@@ -56,7 +62,9 @@ public class NameList {
         }
         return aList;
     }
-
+    
+    //Sorts the entire ArrayList alphabetically
+    
     private ArrayList<Node> sortAlphabetically(ArrayList<Node> aList){
         Node temp;
         for(int i = 1; i < aList.size(); i++){
@@ -71,6 +79,10 @@ public class NameList {
         return aList;
     }
 
+    /*Returns an ArrayList of nodes containing the female and male versions of each name, will need to read
+    the ArrayList in kickstarter and print out data such as gender and occurances for each name.
+    */
+    
     public ArrayList<Node> searchName(String aName){
         ArrayList people = new ArrayList<Node>();
         for(Node node : peopleList){
@@ -80,6 +92,10 @@ public class NameList {
         }
         return people;
     }
+    
+    /*Returns an ArrayList containing first the top 10 girl names, and then the top 10 boy names in the correct order
+    will need to read into the ArrayList in Kickstarter to display needed information
+    */
 
     public ArrayList<Node> mostPopularName() {
         ArrayList<Node> people = new ArrayList<>();
@@ -110,6 +126,8 @@ public class NameList {
         return people;
     }
 
+    //Prints out the entire list of names in alphabetical order, change the print statement to correct data as needed
+    
     public void showNameAlphabetically(){
         ArrayList<Node> sortedList = sortAlphabetically(peopleList);
         for(Node node : sortedList){
